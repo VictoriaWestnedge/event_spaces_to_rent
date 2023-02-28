bf92e3e4476f5ba09bdfe691331591a677a9272c
Victoria Westnedge victoriawestnedge@gmail.com <victoriawestnedge@gmail.com>
2023-02-28 10:40:41 -0300

    added booking controller and nested routes for bookings

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,4 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
   end
 
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
