269219b9a79f8fcf8009fa2ee36ae68c3301752f
fernando ramirez <patricio_129@hotmail.com>
2023-02-28 10:00:27 -0600

    Seed Created

--- app/controllers/event_spaces_controller.rb
+++ app/controllers/event_spaces_controller.rb
@@ -1,9 +1,9 @@
 class EventSpacesController < ApplicationController
   def index
-    @eventspaces =
+    @event_spaces = EventSpace.all
   end
 
   def show
-
+    @event_space = EventSpace.find(params[:id])
   end
 end
