f72c6cf736f32fc8684f9998479a1f01423eaca2
fernando ramirez <patricio_129@hotmail.com>
2023-03-03 07:53:00 -0600

    Agregue el avatar personalizado

--- app/controllers/event_spaces_controller.rb
+++ app/controllers/event_spaces_controller.rb
@@ -1,5 +1,5 @@
 class EventSpacesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :search
+  skip_before_action :authenticate_user!, only: :index
 
   def index
     if params[:query].present?  #busqueda
